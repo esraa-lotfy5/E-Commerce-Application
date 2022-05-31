d6c6f12507ebe813f0fdd6339fff805887bc8988
Esraa Lotfy <83614911+esraa-lotfy5@users.noreply.github.com>
2022-05-31 14:59:52 +0200

    Initial Commit

---
+++ E-Commerce Application/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  E-Commerce Application
+//
+//  Created by Esraa Lotfy  on 5/31/22.
+//  Copyright © 2022 iti. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ E-Commerce ApplicationTests/E_Commerce_ApplicationTests.swift
@@ -0,0 +1,34 @@
+//
+//  E_Commerce_ApplicationTests.swift
+//  E-Commerce ApplicationTests
+//
+//  Created by Esraa Lotfy  on 5/31/22.
+//  Copyright © 2022 iti. All rights reserved.
+//
+
+import XCTest
+@testable import E_Commerce_Application
+
+class E_Commerce_ApplicationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
