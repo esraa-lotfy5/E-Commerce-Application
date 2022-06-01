e29b2cbd8d04bbfb3b9257fdf48a58ce44861f89
Abdelrhman Ahmed <abdelrhmanhsh@gmail.com>
2022-06-01 21:21:33 +0200

    network layer

---
+++ E-Commerce Application/Utils/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  E-Commerce Application
+//
+//  Created by Abdelrhman Ahmed on 01/06/2022.
+//  Copyright © 2022 iti. All rights reserved.
+//
+
+import Foundation
+
+struct ErrorMessages {
+    static let genericError = "Something went wrong!"
+}
