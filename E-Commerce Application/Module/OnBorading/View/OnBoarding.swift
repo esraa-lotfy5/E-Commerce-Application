e773783f166cba25c7108fa51c82bd2017ca5912
NourallahAhmed <NourAllahahmed7001@gmail.com>
2022-06-18 10:38:39 -0700

    project Intro

--- E-Commerce Application/Module/OnBorading/View/OnBoarding.swift
+++ E-Commerce Application/Module/OnBorading/View/OnBoarding.swift
@@ -22,23 +22,23 @@ struct OnBoardingView : View {
 //                           skipBtn: true, alignment: .bottom)
 
 
-                CustomPage(image: "BrandsOnHome", desc: "See Your Favorite Brands ", shownsDismissButton: false, shouldShowOnBoarding: $shouldShowOnBoarding, skipBtn: false, alignment: .bottom)
+                CustomPage(image: "BrandsOnHome", desc: "See Your Favorite Brands ", shownsDismissButton: false, shouldShowOnBoarding: $shouldShowOnBoarding, skipBtn: true, alignment: .bottom)
 
-                CustomPage(image: "SettingsOnHome", desc: "You Can Edit your Settings", shownsDismissButton: false, shouldShowOnBoarding: $shouldShowOnBoarding, skipBtn: false, alignment: .bottom)
+                CustomPage(image: "SettingsOnHome", desc: "You Can Edit your Settings", shownsDismissButton: false, shouldShowOnBoarding: $shouldShowOnBoarding, skipBtn: true, alignment: .bottom)
                 
-                CustomPage(image: "settings", desc: "Change your currency ", shownsDismissButton: false, shouldShowOnBoarding: $shouldShowOnBoarding, skipBtn: false, alignment: .bottom)
+                CustomPage(image: "settings", desc: "Change your currency ", shownsDismissButton: false, shouldShowOnBoarding: $shouldShowOnBoarding, skipBtn: true, alignment: .center)
 
-                CustomPage(image: "filterYourProducts", desc: "Filter the product", shownsDismissButton: false, shouldShowOnBoarding: $shouldShowOnBoarding, skipBtn: false, alignment: .bottom)
+                CustomPage(image: "filterYourProducts", desc: "Filter the product", shownsDismissButton: false, shouldShowOnBoarding: $shouldShowOnBoarding, skipBtn: true, alignment: .center)
 
-                CustomPage(image: "gotoCart",desc: "Tap here to see Your Shopping Cart", shownsDismissButton: false, shouldShowOnBoarding: $shouldShowOnBoarding, skipBtn: false, alignment: .bottom)
+                CustomPage(image: "gotoCart",desc: "Tap here to see Your Shopping Cart", shownsDismissButton: false, shouldShowOnBoarding: $shouldShowOnBoarding, skipBtn: true, alignment: .bottom)
 
-                CustomPage(image: "gotoWishlist",desc: "Tap here to see Your WishList", shownsDismissButton: false , shouldShowOnBoarding: $shouldShowOnBoarding, skipBtn: false, alignment: .bottom)
+                CustomPage(image: "gotoWishlist",desc: "Tap here to see Your WishList", shownsDismissButton: false , shouldShowOnBoarding: $shouldShowOnBoarding, skipBtn: true, alignment: .center)
 
-                CustomPage(image: "seeDescAndRateAndPickQuantity", desc: "See product details and images", shownsDismissButton: false, shouldShowOnBoarding: $shouldShowOnBoarding, skipBtn: false, alignment: .bottom)
+                CustomPage(image: "seeDescAndRateAndPickQuantity", desc: "See product details and images", shownsDismissButton: false, shouldShowOnBoarding: $shouldShowOnBoarding, skipBtn: true, alignment: .bottom)
 
-                CustomPage(image: "choiceSizeAndColor",  desc: "Choice your size and prefer color", shownsDismissButton: false, shouldShowOnBoarding: $shouldShowOnBoarding, skipBtn: false, alignment: .center)
+                CustomPage(image: "choiceSizeAndColor",  desc: "Choice your size and prefer color", shownsDismissButton: false, shouldShowOnBoarding: $shouldShowOnBoarding, skipBtn: true, alignment: .center)
 
-                CustomPage(image: "AddToCartBtn",  desc: "And add the Product on your shopping cart", shownsDismissButton: false, shouldShowOnBoarding: $shouldShowOnBoarding, skipBtn: false, alignment: .bottom)
+                CustomPage(image: "AddToCartBtn",  desc: "And add the Product on your shopping cart", shownsDismissButton: false, shouldShowOnBoarding: $shouldShowOnBoarding, skipBtn: true, alignment: .center)
                 
                 CustomPage(image: "explore the products",  desc: "Lets enjoy the shopping ", shownsDismissButton: true, shouldShowOnBoarding: $shouldShowOnBoarding, skipBtn: false, alignment: .bottom)
                 
@@ -74,52 +74,58 @@ struct CustomPage : View {
         VStack{
         Image( image)
             .resizable()
-            .frame(width: UIScreen.main.bounds.width, height: 600)
+            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height - 150 )
             .padding()
-            .overlay(ImageOverlay(desc: desc), alignment: alignment )
-
-        
-        if shownsDismissButton{
-            Button {
-                shouldShowOnBoarding.toggle()
-            } label: {
-                Text("Get Started")
-                    .bold()
-                    .foregroundColor(.white)
-                    .frame(width: 200, height: 40)
-                    .background(Color.pink)
-                    .cornerRadius(6)
-            }
-        }
+            .overlay(ImageOverlay(desc: desc , shownsDismissButton: shownsDismissButton , shouldShowOnBoarding: $shouldShowOnBoarding), alignment: alignment )
             
-            if skipBtn{
-                Button {
-                    shouldShowOnBoarding.toggle()
-                } label: {
-                    Text("Skip")
-                        .bold()
-                        .foregroundColor(.white)
-                        .frame(width: 200, height: 40)
-                        .background(Color.pink)
-                        .cornerRadius(6)
-                }
-            }
+            
+//
+//            Text(desc)
+//                .font(.system(size: 30))
+//                .padding(6)
+//                .foregroundColor(.white)
+//                .background(Color.pink)
+//                .opacity(0.8)
+//                .cornerRadius(10.0)
+//                .padding()
+            
         }
     }
 }
 struct ImageOverlay: View {
     var desc :String
+    var shownsDismissButton : Bool
+//    var skipBtn : Bool
+    @Binding var shouldShowOnBoarding : Bool
+
     var body: some View {
-        ZStack {
+        VStack {
             Text(desc)
                 .font(.system(size: 30))
                 .padding(6)
+                .foregroundColor(.pink)
+            
+    
+        if shownsDismissButton{
+        Button {
+            shouldShowOnBoarding.toggle()
+        } label: {
+            Text("Get Started")
+                .bold()
                 .foregroundColor(.white)
-        }.background(Color.gray)
+                .frame(width: 200, height: 40)
+                .background(Color.pink)
+                .cornerRadius(6)
+        }
+    }
+    
+            
+        }.background(Color.white)
         .opacity(0.8)
         .cornerRadius(10.0)
         .padding()
     }
+    
 }
 //nourallahahmed1100@gamil.com
 //password
